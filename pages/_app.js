c54bbf7c0bb70ff2c4012044272b17eb4c7e2b41
Payton Chertude <p.chertude34@gmail.com>
2021-04-07 21:07:36 -0700

    add chakra-ui and hero section

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/main.scss';
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../chakra.theme";
+
+import "../styles/main.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
