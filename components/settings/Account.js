f2cbb76d5c5a2679a7a76be034354a81bce9b226
Michael Hoobler <hoobler.michael@gmail.com>
2021-04-14 16:20:45 -0400

    Feature/settings (#10)
    * started making seperate sub-pages
    
    * trying some stuff with forms
    
    * finished styling, cleaned up some stuff, and memoized search

---
+++ components/settings/Account.js
@@ -0,0 +1,280 @@
+import { useState, useEffect, useMemo } from 'react';
+import {
+  Stack,
+  Flex,
+  Seperator,
+  Box,
+  ButtonGroup,
+  Button,
+  IconButton,
+  Text,
+  Heading,
+  Grid,
+  GridItem,
+  FormControl,
+  FormLabel,
+  Input,
+} from '@chakra-ui/react';
+import { SettingsIcon, CloseIcon, AddIcon } from '@chakra-ui/icons';
+
+export default function Account() {
+  const [userData, setUserData] = useState({});
+  const [loadForm, setLoadForm] = useState(false);
+
+  useEffect(() => {
+    mockAPI
+      .getUserData()
+      .then((res) => setUserData(res))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleLoadForm = () => {
+    setLoadForm(!loadForm);
+  };
+
+  const handleSubmitEdit = (editData) => {
+    if (editData === null) {
+      return setLoadForm(false);
+    }
+    mockAPI
+      .putUserData(editData)
+      .then((res) => setUserData(res))
+      .catch((err) => console.log(err))
+      .finally(() => setLoadForm(false));
+  };
+
+  if (userData.interests) {
+    return (
+      <Stack>
+        <Flex mb="3%">
+          <Heading>Account</Heading>
+          <IconButton
+            icon={<SettingsIcon />}
+            ml="auto"
+            bgGradient={'linear(to-r, primary.300, primary.500)'}
+            color="white"
+            onClick={handleLoadForm}
+            _hover={{
+              color: 'white',
+              bgGradient: 'linear(to-r, primary.400, primary.600)',
+            }}
+          />
+        </Flex>
+        {loadForm ? (
+          <EditForm data={userData} handleSubmit={handleSubmitEdit} />
+        ) : (
+          <Grid minW="50%" templateColumns="repeat(10, 1fr)" gap={12}>
+            <GridItem colSpan={1}>
+              <Text as={'span'}>Title: </Text>
+            </GridItem>
+            <GridItem colSpan={9}>
+              <Text as={'span'}>{userData.title}</Text>
+            </GridItem>
+            <GridItem colSpan={1}>
+              <Text as={'span'}>Interests: </Text>
+            </GridItem>
+            <GridItem colSpan={9}>
+              <Text as={'span'}>{userData.interests.join(', ')}</Text>
+            </GridItem>
+            <GridItem colSpan={1}>
+              <Text as={'span'}>Linkedin: </Text>
+            </GridItem>
+            <GridItem colSpan={9}>
+              <Text as={'span'}>{userData.linkedin_url}</Text>
+            </GridItem>
+          </Grid>
+        )}
+      </Stack>
+    );
+  }
+
+  return (
+    <Stack>
+      <Flex>
+        <Heading>Uh oh, looks like something went wrong</Heading>
+      </Flex>
+    </Stack>
+  );
+}
+
+function EditForm({ data, handleSubmit }) {
+  const [editData, setEditData] = useState(data);
+  const [interestSearch, setInterestSearch] = useState('');
+  const interestSet = new Set(editData.interests);
+
+  const handleChange = ({ name, value }) => {
+    setEditData({ ...editData, [name]: value });
+  };
+
+  const handleAddInterest = (str) => {
+    interestSet.add(str);
+    setEditData({
+      ...editData,
+      interests: [...interestSet],
+    });
+  };
+
+  const handleRemoveInterest = (str) => {
+    setEditData({
+      ...editData,
+      interests: editData.interests.filter((e) => e !== str),
+    });
+  };
+
+  const handleInterestSearch = useMemo(() => {
+    if (interestSearch === '') {
+      return null;
+    }
+    let resultsArray = INTERESTS_VALUES.filter(
+      (e) =>
+        e.toLowerCase().includes(interestSearch.toLowerCase()) &&
+        !interestSet.has(e),
+    );
+
+    return resultsArray.length === 0 ? (
+      <Text fontWeight={'bold'}>No results</Text>
+    ) : (
+      resultsArray.map((e) => {
+        return (
+          <ButtonGroup key={e} mr="6px" size="sm" isAttached>
+            <Button
+              bg="blue.300"
+              _hover={{
+                cursor: 'default',
+                bgColor: 'blue.300',
+              }}
+            >
+              {e}
+            </Button>
+            <IconButton
+              icon={<AddIcon />}
+              bg="blue.300"
+              _hover={{
+                bgColor: 'primary.300',
+              }}
+              onClick={() => {
+                handleAddInterest(e);
+              }}
+            />
+          </ButtonGroup>
+        );
+      })
+    );
+  }, [interestSearch, editData.interests]);
+
+  return (
+    <Stack as="form" spacing="12px" maxW={{ base: '100%', md: '50%' }}>
+      <FormControl>
+        <FormLabel fontWeight="bold" htmlFor="user-title">
+          Title:
+        </FormLabel>
+        <Input
+          name="title"
+          id="user-title"
+          defaultValue={editData.title}
+          onChange={(evt) => handleChange(evt.currentTarget)}
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel fontWeight="bold" htmlFor="user-title">
+          Interests:
+        </FormLabel>
+        <Flex flexWrap="wrap">
+          <Input
+            minW="100%"
+            mb="6px"
+            name="interests-search"
+            id="user-title"
+            value={interestSearch}
+            onChange={(evt) => setInterestSearch(evt.currentTarget.value)}
+          />
+          {/* Currently Selected Interests */}
+          {editData.interests.map((e) => (
+            <ButtonGroup key={e} mr="6px" size="sm" isAttached>
+              <Button
+                bg="primary.300"
+                _hover={{
+                  cursor: 'default',
+                  bgColor: 'primary.300',
+                }}
+              >
+                {e}
+              </Button>
+              <IconButton
+                bg="primary.300"
+                icon={<CloseIcon />}
+                _hover={{
+                  bgColor: 'red.300',
+                }}
+                onClick={() => {
+                  handleRemoveInterest(e);
+                }}
+              />
+            </ButtonGroup>
+          ))}
+          {/* Interest Search Results (not currently selected) */}
+          {handleInterestSearch}
+        </Flex>
+      </FormControl>
+      <FormControl>
+        <FormLabel fontWeight="bold" htmlFor="user-title">
+          Linkedin:
+        </FormLabel>
+        <Input
+          name="linkedin_url"
+          id="user-title"
+          value={editData.linkedin_url}
+          onChange={(evt) => handleChange(evt.currentTarget)}
+        />
+      </FormControl>
+      <Flex role="group">
+        <Button
+          mr="12px"
+          bg="blue.400"
+          color="white"
+          name="form-submit"
+          onClick={() => handleSubmit(editData)}
+        >
+          Submit
+        </Button>
+        <Button
+          name="form-cancel"
+          bg="red.400"
+          color="white"
+          onClick={() => handleSubmit(null)}
+        >
+          Cancel
+        </Button>
+      </Flex>
+    </Stack>
+  );
+}
+
+var mockUserData = {
+  title: 'Fullstack Web Developer',
+  interests: ['Frontend', 'Design', 'Javascript'],
+  linkedin_url: 'https://www.linkedin.com/in/michael-hoobler-090847174/',
+};
+
+const mockAPI = {
+  getUserData: () => new Promise((res) => res(mockUserData)),
+  putUserData: (data) => {
+    mockUserData = {
+      ...mockUserData,
+      ...data,
+    };
+
+    return new Promise((res) => res(mockUserData));
+  },
+};
+
+const INTERESTS_VALUES = [
+  'Frontend',
+  'Backend',
+  'Design',
+  'Javascript',
+  'Vue',
+  'Django',
+  'AWS',
+  'GCP',
+];
--- pages/settings/[menu].js
+++ pages/settings/[menu].js
@@ -9,31 +9,50 @@ import {
   Link,
   ButtonGroup,
   Button,
+  useColorModeValue,
 } from '@chakra-ui/react';
 
+import Account from '../../components/settings/Account';
+import Security from '../../components/settings/Security';
 import Navbar from '../../components/Navbar';
 
-const ButtonLink = ({ href, children }) => {
+const LoadView = (menu) => {
+  switch (menu) {
+    case 'account': {
+      return <Account />;
+    }
+    case 'security': {
+      return <Security />;
+    }
+    default:
+      return null;
+  }
+};
+
+const ButtonLink = (props) => {
+  const { children } = props;
+
   return (
     <Link
-      href={href}
       align="center"
       bgGradient={'linear(to-r, primary.300, primary.500)'}
       fontWeight="bold"
       borderRadius="md"
-      p="1"
+      p="2"
+      minW="80%"
       color="white"
       _hover={{
         color: 'white',
         bgGradient: 'linear(to-r, primary.400, primary.600)',
       }}
+      {...props}
     >
       {children}
     </Link>
   );
 };
 
-const Profile = () => {
+export default function Settings() {
   const router = useRouter();
   const { menu } = router.query;
 
@@ -42,28 +61,22 @@ const Profile = () => {
       <Navbar />
       <Flex flex="1">
         <Grid minW="100%" templateColumns="repeat(10, 1fr)">
+          {/* Menu Sidebar*/}
           <GridItem
             bgGradient={'linear(to-br, dark.600, dark.900)'}
             colSpan={{ base: 10, md: 2 }}
           >
-            {/* Menu Sidebar*/}
-            <Stack p={8}>
-              {/* Should make ActiveLink component */}
+            <Stack align="center" py={8}>
               <ButtonLink href="account">Account</ButtonLink>
-              <ButtonLink href="test">Test</ButtonLink>
+              <ButtonLink href="security">Security</ButtonLink>
             </Stack>
           </GridItem>
           {/* Inputs Main */}
-          {/* Should load in selected menu component here */}
-          <GridItem colSpan={{ base: 10, md: 8 }}>
-            <Stack p={8}>
-              <Text>{menu}</Text>
-            </Stack>
+          <GridItem p={8} colSpan={{ base: 10, md: 8 }}>
+            {LoadView(menu)}
           </GridItem>
         </Grid>
       </Flex>
     </Flex>
   );
-};
-
-export default Profile;
+}
