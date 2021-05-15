093000781365c5d83b7c7c209baca63d5c0989e1
Payton Chertude <p.chertude34@gmail.com>
2021-05-15 09:17:33 -0700

    add conditional layout rendering

--- pages/_app.js
+++ pages/_app.js
@@ -7,12 +7,15 @@ import SiteLayout from "../components/SiteLayout";
 import "../styles/main.scss";
 
 function MyApp({ Component, pageProps }) {
+  // Conditionally render layout based on the component that we are rendering.
+  // For more information about this, check out https://adamwathan.me/2019/10/17/persistent-layout-patterns-in-nextjs/
+  const Layout = Component.layout || SiteLayout;
   return (
     <ChakraProvider theme={theme}>
       <AuthProvider session={pageProps.session}>
-        <SiteLayout>
+        <Layout>
           <Component {...pageProps} />
-        </SiteLayout>
+        </Layout>
       </AuthProvider>
     </ChakraProvider>
   );
