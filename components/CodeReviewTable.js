a7f1e3a9d4087fabf8941877b6942e97beafaba4
Payton Chertude <p.chertude34@gmail.com>
2021-05-28 11:15:38 -0600

    fix mobile style for code review table

--- components/CodeReviewTable.js
+++ components/CodeReviewTable.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   Badge,
+  Box,
   Button,
   Table,
   Thead,
@@ -9,35 +10,38 @@ import {
   Tr,
   Th,
   Td,
+  Container,
 } from "@chakra-ui/react";
 
 function CodeReviewTable(props) {
   return (
-    <Table>
-      <Thead>
-        <Tr>
-          <Th>Phase</Th>
-          <Th>Date Submitted</Th>
-          <Th>Status</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        <Tr>
-          <Td>Phase 1 - Setup</Td>
-          <Td>05/20/2021</Td>
-          <Td>
-            <Badge colorScheme="green">Done</Badge>
-          </Td>
-        </Tr>
-        <Tr>
-          <Td>Phase 2 - View Item</Td>
-          <Td>05/27/2021</Td>
-          <Td>
-            <Badge colorScheme="yellow">In Progress</Badge>
-          </Td>
-        </Tr>
-      </Tbody>
-    </Table>
+    <Box maxW="100%" overflow="scroll">
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>Phase</Th>
+            <Th>Date Submitted</Th>
+            <Th>Status</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>Phase 1 - Setup</Td>
+            <Td>05/20/2021</Td>
+            <Td>
+              <Badge colorScheme="green">Done</Badge>
+            </Td>
+          </Tr>
+          <Tr>
+            <Td>Phase 2 - View Item</Td>
+            <Td>05/27/2021</Td>
+            <Td>
+              <Badge colorScheme="yellow">In Progress</Badge>
+            </Td>
+          </Tr>
+        </Tbody>
+      </Table>
+    </Box>
   );
 }
 
